24e5140df1b91238911e0f687c883d182e774e33
albertroig89 <albertroiglg@gmail.com>
2021-02-05 19:33:46 +0100

    mitant tema 14

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Profession;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,17 +13,25 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
+//        DB:insert('INSERT INTO professions (title) VALUES (:title)'. [
+//            'title' => 'Desarrollador back-end',
+//        ]);                                        //Es el mateix escrit en sql
+//      
+//      
+//        DB::table('professions')->insert([
+//           'title' => 'Desarrollador back-end',
+//        ]);                                       //Es el mateix escrit en lo constructor de consultes
         
-        DB::table('professions')->insert([
-           'title' => 'Desarrollador back-end',
+        \App\Models\Profession::create([
+            'title' => 'Desarrollador back-end', 
+        ]);                                         //Es el mateix escrit en models de laravel
+        
+        Profession::create([                        //Si posem use App\Models\Profession; al principi del php no cal que ho posem cada cop com a l'anterior cas
+            'title' => 'Desarrollador front-end',
         ]);
-        
-        DB::table('professions')->insert([
-           'title' => 'Desarrollador front-end',
-        ]);
-        
-        DB::table('professions')->insert([
-           'title' => 'Desarrollador web',
+                
+        Profession::create([
+            'title' => 'Desarrollador web',
         ]);
     }
 }
