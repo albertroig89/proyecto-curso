a18f176e93d9c7aa590e84e0daf19c279476f8af
albertroig89 <albertroiglg@gmail.com>
2021-03-08 18:19:46 +0100

    curs laravel desde 0 finalitzat

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,99 +4,112 @@
 
 @section('content')
 
-    <h1>{{ $title }}</h1>
+    <div class="card" >
+        <div class="card-header"><h3>{{ $title }}</h3></div>
+        <div class="card-body">
+            <form method="POST" action="{{ url('usuarios') }}">
 
-    <form method="POST" action="{{ url('usuarios') }}">
+                {!! csrf_field() !!}
 
-            {!! csrf_field() !!}
+                <div class="form">
+                    <div class="col-md-4 mb-3">
+                        @if ($errors->has('name'))
+                            <label for="name">Nombre:</label>
+                            <input type="text" name="name" class="form-control is-invalid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name') }}">
+                            <div class="invalid-feedback">
+                                {{ $errors->first('name') }}
+                            </div>
+                        @elseif ($errors->any())
+                            <label for="name">Nombre:</label>
+                            <input type="text" name="name" class="form-control is-valid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name') }}">
+                            <div class="valid-feedback">
+                                Correcto!
+                            </div>
+                        @else
+                            <label for="name">Nombre:</label>
+                            <input type="text" name="name" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name') }}">
+                            <small id="nameHelp" class="form-text text-muted">Escribe tu nombre.</small>
+                        @endif
+                    </div>
 
-        <div class="form">
-            <div class="col-md-4 mb-3">
-                @if ($errors->has('name'))
-                    <label for="name">Nombre:</label>
-                    <input type="text" name="name" class="form-control is-invalid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name') }}">
-                    <div class="invalid-feedback">
-                        {{ $errors->first('name') }}
+                    <div class="col-md-4 mb-3">
+                        @if ($errors->has('email'))
+                            <label for="email">Correo electronico:</label>
+                            <input type="email" name="email" class="form-control is-invalid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email') }}">
+                            <div class="invalid-feedback">
+                                {{ $errors->first('email') }}
+                            </div>
+                        @elseif ($errors->any())
+                            <label for="email">Correo electronico:</label>
+                            <input type="email" name="email" class="form-control is-valid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email') }}">
+                            <div class="valid-feedback">
+                                Correcto!
+                            </div>
+                        @else
+                            <label for="email">Correo electronico:</label>
+                            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email') }}">
+                            <small id="emailHelp" class="form-text text-muted">Escribe un email que puedas verificar.</small>
+                        @endif
                     </div>
-                @elseif ($errors->any())
-                    <label for="name">Nombre:</label>
-                    <input type="text" name="name" class="form-control is-valid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name') }}">
-                    <div class="valid-feedback">
-                        Correcto!
+{{--                    <div class="col-md-4 mb-3">--}}
+{{--                        <select class="custom-select" required>--}}
+{{--                            <option value="">Selecciona tu profession</option>--}}
+{{--                            @foreach ($professions as $profession)--}}
+{{--                                <option value="{{ $profession->id }}">{{ $profession->title }}</option>--}}
+{{--                            @endforeach--}}
+{{--                        </select>--}}
+{{--                        <div class="invalid-feedback">Example invalid custom select feedback</div>--}}
+{{--                    </div> VOLIA FER UNA SELECCIO DE LA PROFESSIO AL CREAR O EDITAR USUARIS--}}
+                    <div class="col-md-4 mb-3">
+                        @if ($errors->has('password'))
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control is-invalid" id="password" placeholder="Introduce tu contraseña">
+                            <div class="invalid-feedback">
+                                {{ $errors->first('password') }}
+                            </div>
+                        @else
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Introduce tu contraseña">
+                            <small id="emailHelp" class="form-text text-muted">Minimo 6 caracteres.</small>
+                        @endif
                     </div>
-                @else
-                    <label for="name">Nombre:</label>
-                    <input type="text" name="name" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name') }}">
-                    <small id="nameHelp" class="form-text text-muted">Escribe tu nombre.</small>
-               @endif
-            </div>
+                </div>
+                {{--                <div class="form-group">--}}
+                {{--                        <div class="form-check">--}}
+                {{--                                <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>--}}
+                {{--                                <label class="form-check-label" for="invalidCheck3">--}}
+                {{--                                        Agree to terms and conditions--}}
+                {{--                                </label>--}}
+                {{--                                --}}
+                {{--                                <div class="invalid-feedback">--}}
+                {{--                                        @if ($errors->has('checkbox'))--}}
+                {{--                                                <div class="invalid-feedback">--}}
+                {{--                                                        <p>{{ $errors->first('password') }}</p>--}}
+                {{--                                                </div>--}}
+                {{--                                        @endif--}}
+                {{--                                </div>--}}
+                {{--                        </div>--}}
+                {{--                </div>--}}
+                <button type="submit" class="btn btn-primary">Crear usuario</button>
+            </form>
+            <br>
+            <p><a href="{{ route('users.index') }}">Regresar al listado de usuarios</a></p>
 
-            <div class="col-md-4 mb-3">
-                @if ($errors->has('email'))
-                    <label for="email">Correo electronico:</label>
-                    <input type="email" name="email" class="form-control is-invalid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email') }}">
-                    <div class="invalid-feedback">
-                        {{ $errors->first('email') }}
+            @if ($errors->any())
+                <br>
+                <div class="col-md-4 mb-3">
+                    <div class="alert alert-danger">
+                        <h5>Por favor corrige los errores mencionados arriba</h5>
+                        {{--                    <ul>--}}
+                        {{--                        @foreach ($errors->all() as $error)--}}
+                        {{--                            <li>{{ $error }}</li>--}}
+                        {{--                        @endforeach--}}
+                        {{--                    </ul>--}}
                     </div>
-                @elseif ($errors->any())
-                    <label for="email">Correo electronico:</label>
-                    <input type="email" name="email" class="form-control is-valid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email') }}">
-                    <div class="valid-feedback">
-                        Correcto!
-                    </div>
-                @else
-                    <label for="email">Correo electronico:</label>
-                    <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email') }}">
-                    <small id="emailHelp" class="form-text text-muted">Escribe un email que puedas verificar.</small>
-                @endif
-            </div>
-            <div class="col-md-4 mb-3">
-                @if ($errors->has('password'))
-                    <label for="password">Password</label>
-                    <input type="password" name="password" class="form-control is-invalid" id="password" placeholder="Introduce tu contraseña">
-                    <div class="invalid-feedback">
-                            {{ $errors->first('password') }}
-                    </div>
-                @else
-                    <label for="password">Password</label>
-                    <input type="password" name="password" class="form-control" id="password" placeholder="Introduce tu contraseña">
-                    <small id="emailHelp" class="form-text text-muted">Minimo 6 caracteres.</small>
-                @endif
-            </div>
+                </div>
+            @endif
         </div>
-{{--                <div class="form-group">--}}
-{{--                        <div class="form-check">--}}
-{{--                                <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>--}}
-{{--                                <label class="form-check-label" for="invalidCheck3">--}}
-{{--                                        Agree to terms and conditions--}}
-{{--                                </label>--}}
-{{--                                --}}
-{{--                                <div class="invalid-feedback">--}}
-{{--                                        @if ($errors->has('checkbox'))--}}
-{{--                                                <div class="invalid-feedback">--}}
-{{--                                                        <p>{{ $errors->first('password') }}</p>--}}
-{{--                                                </div>--}}
-{{--                                        @endif--}}
-{{--                                </div>--}}
-{{--                        </div>--}}
-{{--                </div>--}}
-            <button type="submit" class="btn btn-primary">Crear usuario</button>
-    </form>
-    <br>
-    <p><a href="{{ route('users.index') }}">Regresar al listado de usuarios</a></p>
+    </div>
 
-    @if ($errors->any())
-        <br>
-        <div class="col-md-4 mb-3">
-            <div class="alert alert-danger">
-                <h5>Por favor corrige los errores mencionados arriba</h5>
-                {{--                    <ul>--}}
-                {{--                        @foreach ($errors->all() as $error)--}}
-                {{--                            <li>{{ $error }}</li>--}}
-                {{--                        @endforeach--}}
-                {{--                    </ul>--}}
-            </div>
-        </div>
-    @endif
 
 @endsection
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,84 +4,86 @@
 
 @section('content')
 
-        <h1>{{ $title }}</h1>
-
-        <form method="POST" action="{{ url("usuarios/{$user->id}") }}">
+    <div class="card">
+        <div class="card-header"><h3>{{ $title }}</h3></div>
+        <div class="card-body">
+            <form method="POST" action="{{ url("usuarios/{$user->id}") }}">
                 {{ method_field('PUT') }}
                 {!! csrf_field() !!}
 
                 <div class="form">
-                        <div class="col-md-4 mb-3">
-                                @if ($errors->has('name'))
-                                        <label for="name">Nombre:</label>
-                                        <input type="text" name="name" class="form-control is-invalid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name', $user->name) }}">
-                                        <div class="invalid-feedback">
-                                                {{ $errors->first('name') }}
-                                        </div>
-                                @elseif ($errors->any())
-                                        <label for="name">Nombre:</label>
-                                        <input type="text" name="name" class="form-control is-valid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name', $user->name) }}">
-                                        <div class="valid-feedback">
-                                                Correcto!
-                                        </div>
-                                @else
-                                        <label for="name">Nombre:</label>
-                                        <input type="text" name="name" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name', $user->name) }}">
-                                        <small id="nameHelp" class="form-text text-muted">Escribe tu nombre.</small>
-                                @endif
-                        </div>
+                    <div class="col-md-4 mb-3">
+                        @if ($errors->has('name'))
+                            <label for="name">Nombre:</label>
+                            <input type="text" name="name" class="form-control is-invalid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name', $user->name) }}">
+                            <div class="invalid-feedback">
+                                {{ $errors->first('name') }}
+                            </div>
+                        @elseif ($errors->any())
+                            <label for="name">Nombre:</label>
+                            <input type="text" name="name" class="form-control is-valid" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name', $user->name) }}">
+                            <div class="valid-feedback">
+                                Correcto!
+                            </div>
+                        @else
+                            <label for="name">Nombre:</label>
+                            <input type="text" name="name" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Bertito tito" value="{{ old('name', $user->name) }}">
+                            <small id="nameHelp" class="form-text text-muted">Escribe tu nombre.</small>
+                        @endif
+                    </div>
 
-                        <div class="col-md-4 mb-3">
-                                @if ($errors->has('email'))
-                                        <label for="email">Correo electronico:</label>
-                                        <input type="email" name="email" class="form-control is-invalid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email', $user->email) }}">
-                                        <div class="invalid-feedback">
-                                                {{ $errors->first('email') }}
-                                        </div>
-                                @elseif ($errors->any())
-                                        <label for="email">Correo electronico:</label>
-                                        <input type="email" name="email" class="form-control is-valid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email', $user->email) }}">
-                                        <div class="valid-feedback">
-                                                Correcto!
-                                        </div>
-                                @else
-                                        <label for="email">Correo electronico:</label>
-                                        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email', $user->email) }}">
-                                        <small id="emailHelp" class="form-text text-muted">Escribe un email que puedas verificar.</small>
-                                @endif
-                        </div>
-                        <div class="col-md-4 mb-3">
-                                @if ($errors->has('password'))
-                                        <label for="password">Password</label>
-                                        <input type="password" name="password" class="form-control is-invalid" id="password" placeholder="Introduce tu contraseña">
-                                        <div class="invalid-feedback">
-                                                {{ $errors->first('password') }}
-                                        </div>
-                                @else
-                                        <label for="password">Password</label>
-                                        <input type="password" name="password" class="form-control" id="password" placeholder="Introduce tu contraseña">
-                                        <small id="emailHelp" class="form-text text-muted">Minimo 6 caracteres.</small>
-                                @endif
-                        </div>
+                    <div class="col-md-4 mb-3">
+                        @if ($errors->has('email'))
+                            <label for="email">Correo electronico:</label>
+                            <input type="email" name="email" class="form-control is-invalid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email', $user->email) }}">
+                            <div class="invalid-feedback">
+                                {{ $errors->first('email') }}
+                            </div>
+                        @elseif ($errors->any())
+                            <label for="email">Correo electronico:</label>
+                            <input type="email" name="email" class="form-control is-valid" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email', $user->email) }}">
+                            <div class="valid-feedback">
+                                Correcto!
+                            </div>
+                        @else
+                            <label for="email">Correo electronico:</label>
+                            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="bertito@example.es" value="{{ old('email', $user->email) }}">
+                            <small id="emailHelp" class="form-text text-muted">Escribe un email que puedas verificar.</small>
+                        @endif
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        @if ($errors->has('password'))
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control is-invalid" id="password" placeholder="Introduce tu contraseña">
+                            <div class="invalid-feedback">
+                                {{ $errors->first('password') }}
+                            </div>
+                        @else
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Introduce tu contraseña">
+                            <small id="emailHelp" class="form-text text-muted">Minimo 6 caracteres.</small>
+                        @endif
+                    </div>
                 </div>
 
                 <button type="submit" class="btn btn-primary">Editar usuario</button>
-        </form>
-        <br>
-        <p><a href="{{ route('users.index') }}">Regresar al listado de usuarios</a></p>
+            </form>
+            <br>
+            <p><a href="{{ route('users.index') }}">Regresar al listado de usuarios</a></p>
 
-        @if ($errors->any())
+            @if ($errors->any())
                 <br>
                 <div class="col-md-4 mb-3">
-                        <div class="alert alert-danger">
-                                <h5>Por favor corrige los errores mencionados arriba</h5>
-                                {{--                    <ul>--}}
-                                {{--                        @foreach ($errors->all() as $error)--}}
-                                {{--                            <li>{{ $error }}</li>--}}
-                                {{--                        @endforeach--}}
-                                {{--                    </ul>--}}
-                        </div>
+                    <div class="alert alert-danger">
+                        <h5>Por favor corrige los errores mencionados arriba</h5>
+                        {{--                    <ul>--}}
+                        {{--                        @foreach ($errors->all() as $error)--}}
+                        {{--                            <li>{{ $error }}</li>--}}
+                        {{--                        @endforeach--}}
+                        {{--                    </ul>--}}
+                    </div>
                 </div>
-        @endif
-
+            @endif
+        </div>
+    </div>
 @endsection
