c0e4730cbb8733539396f6de4295e3297d95f546
albertroig89 <albertroiglg@gmail.com>
2023-06-14 15:13:54 +0200

    tema 13 panel de control finalitzat

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use App\Profession;
-use App\Skill;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,15 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::component('shared._card', 'card');
 
-        View::composer(['users.create', 'users.edit', 'shared._professionerror'], function ($view) {
-            $professions = Profession::orderBy('title', 'ASC')->get();
-            $skills = Skill::orderBy('name', 'ASC')->get();
-            $roles = trans('users.roles');
-
-            $view->with(compact('professions', 'skills', 'roles'));
-        });
     }
 
     /**
